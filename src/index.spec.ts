675bc5544a83d946fa2126b9a9a78e9445419e3c
emi190396 <emily.lorenz@oliverlorenz.com>
2020-10-13 11:58:07 +0200

    chore: changes tests

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,15 +1,22 @@
 import { FsUnzipper } from '.';
-import { existsSync } from 'fs';
+import { existsSync, unlinkSync } from 'fs';
 
 describe(FsUnzipper.name, () => {
+	beforeEach(() => {
+		unlinkSync('test/1.txt');
+		unlinkSync('test/2.txt');
+	  });
+	
 	it('should return promise', async () => {
-		const processor = new FsUnzipper();
-		await expect(
-			processor.process('test/multipleFiles.zip', 'test')
-		).resolves.toBeUndefined();
+	 const processor = new FsUnzipper();
+	 await expect(
+	 processor.process('test/multipleFiles.zip', 'test')
+	 ).resolves.toBeUndefined();
 	});
 
-	it('should write files in test directory', () => {
+	it('should write files in test directory', async () => {
+		const processor = new FsUnzipper();
+		await processor.process('test/multipleFiles.zip', 'test')
 		expect(existsSync('test/1.txt')).toBeTruthy();
 	});
 });
